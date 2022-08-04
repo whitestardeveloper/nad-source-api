cf121abe24557e15db365407c9a5a0a0af8f9efd
Abdülkadir AKYILDIZ <abdulkadir.akyildiz@aurorabilisim.com>
2022-08-04 23:14:17 +0300

    static files directory change

--- ws_source/settings.py
+++ ws_source/settings.py
@@ -210,7 +210,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 
